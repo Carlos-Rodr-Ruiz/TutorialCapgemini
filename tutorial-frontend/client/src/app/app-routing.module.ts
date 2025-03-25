c08204bf62e17aa59ec3e1baf94b08dfea836c94
Rodriguez Ruiz external <carlos.rodriguez-ruiz.external@capgemini.com>
2025-03-25 13:26:32 +0100

    Code: Listado paginado-Completado

--- tutorial-frontend/client/src/app/app-routing.module.ts
+++ tutorial-frontend/client/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryListComponent } from './category/category-list/category-list.component';
+import { AuthorListComponent } from './author/author-list/author-list.component';
 
 const routes: Routes = [
-  { path: 'categories', component: CategoryListComponent },
+    { path: 'categories', component: CategoryListComponent },
+    { path: 'authors', component: AuthorListComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- tutorial-frontend/client/src/app/app.module.ts
+++ tutorial-frontend/client/src/app/app.module.ts
@@ -3,8 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CoreModule } from './core/core.module';
 import { CategoryModule } from './category/category.module';
+
+import { AuthorModule } from './author/author.module';
+
 
 
 
@@ -17,6 +22,8 @@ import { CategoryModule } from './category/category.module';
     AppRoutingModule,
     CoreModule,
     CategoryModule,
+    AuthorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
